e28f239e986ca403a1920106ad636ef0bddf739b
shkch02 <cba7215@gmail.com>
2025-11-06 20:26:09 +0900

    get_aliases 구현 완료 및 config.py에 opne 추가 (하기싫었는데 man 2 open에 opneat2가 안써있음 ㅠㅠ)

--- config.py
+++ config.py
@@ -31,6 +31,11 @@ MANUAL_MAP = {
 MANUAL_ALIAS_MAP = {
     # key: 대표 이름 (glibc 함수)
     # value: 함께 추적해야 할 syscalls (주로 실제 kernel syscall)
+
+        # open(2) 계열 -> sys_openat, man 2 open에 openat2가 없어서 여기서 수동으로 조정
+    'open': ['open', 'openat', 'creat'],
+    'openat': ['open', 'openat', 'creat'],
+    'creat': ['open', 'openat', 'creat'],
     
     # select(2) 계열 -> sys_pselect6
     # VALID_SYSCALLS에 'pselect6'가 있다고 가정합니다. 
